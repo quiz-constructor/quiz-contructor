5f87bacbc3dfa5073c5ee6f5584119a1597da2de
k05m0navt <gubaydullin.dan@gmail.com>
2022-03-01 21:14:31 +0300

    Add test image

--- src/components/QuizImage/QuizImage.tsx
+++ src/components/QuizImage/QuizImage.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react';
 
-export type QuizImageProps = {};
+export type QuizImageProps = {
+    src: string;
+};
 
-const QuizImage: FC<QuizImageProps> = () => {
-    return <div>QuizImage</div>;
+const QuizImage: FC<QuizImageProps> = ({ src }) => {
+    return (
+        <div className='quiz-image flex flex-wrap h-screen justify-center content-center'>
+            <img className='max-w-lg h-auto shadow-lg' src={src} alt='quiz' />
+        </div>
+    );
 };
 
 export default QuizImage;
--- src/pages/QuizPage/QuizPage.tsx
+++ src/pages/QuizPage/QuizPage.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
+import { QuizImage } from '../../components';
 
 export type QuizPageProps = {};
 
 const QuizPage: FC<QuizPageProps> = () => {
-    return <div>QuizPage</div>;
+    return (
+        <div className='quiz-page'>
+            <QuizImage src='https://mdbootstrap.com/img/new/slides/041.jpg' />
+        </div>
+    );
 };
 
 export default QuizPage;
